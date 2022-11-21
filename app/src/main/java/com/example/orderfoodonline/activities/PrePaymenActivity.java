61953a9588d0465ac1e7f95706ecdf06159911c5
Taiemcute <85283737+Taiem15032002@users.noreply.github.com>
2022-11-21 09:10:03 +0700

    thanh toans momo v1

--- app/src/main/java/com/example/orderfoodonline/activities/PrePaymenActivity.java
+++ app/src/main/java/com/example/orderfoodonline/activities/PrePaymenActivity.java
@@ -3,27 +3,54 @@ package com.example.orderfoodonline.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.orderfoodonline.R;
 import com.example.orderfoodonline.Utils.Utils;
+import com.example.orderfoodonline.retrofit.FoodAppApi;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import io.paperdb.Paper;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import vn.momo.momo_partner.AppMoMoLib;
 
 public class PrePaymenActivity extends AppCompatActivity {
 
     TextView tvuser, tvsdt, tvtongtienthanhtoan, back_container;
     RadioButton rd50per, rd100per;
     AppCompatButton btnMomo, btnZalo;
+    EditText edtdiachi;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    FoodAppApi foodAppApi;
+
+    private String amount = "10000";
+    private String fee = "0";
+    int environment = 0;//developer default
+    private String merchantName = "Expected Food";
+    private String merchantCode = "SCB01";
+    private String merchantNameLabel = "Nhà cung cấp";
+    private String description = "Thanh toán món ăn online";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_paymen);
+
+//        AppMoMoLib.getInstance().setEnvironment(AppMoMoLib.ENVIRONMENT.DEVELOPMENT); // AppMoMoLib.ENVIRONMENT.PRODUCTION
         Paper.init(this);
         initView();
         initControl();
@@ -44,6 +71,29 @@ public class PrePaymenActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnMomo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str_diachi = edtdiachi.getText().toString().trim();
+                if (TextUtils.isEmpty(str_diachi)){
+                    android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(v.getRootView().getContext());
+                    builder.setTitle("Thông báo");
+                    builder.setMessage("Hãy nhập địa chỉ giao hàng!");
+                    builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.show();
+                }else{
+                    String str_email = Utils.user_current.getEmail();
+                    String str_sdt = Utils.user_current.getMobile();
+                    int id = Utils.user_current.getId();
+//                    compositeDisposable.add(foodAppApi.createOrder(str_email, str_sdt,String.valueOf(tvtongtienthanhtoan),id,str_diachi,new Gson().toJson()))
+                }
+            }
+        });
     }
 
     private void initView() {
@@ -51,5 +101,96 @@ public class PrePaymenActivity extends AppCompatActivity {
         tvsdt = findViewById(R.id.tv_sdt);
         back_container = findViewById(R.id.back_container);
         tvtongtienthanhtoan = findViewById(R.id.tv_tongtienthanhtoan);
+        btnMomo = findViewById(R.id.btn_momo);
+        edtdiachi = findViewById(R.id.edtdiachi);
     }
+
+    //Get token through MoMo app
+    private void requestPayment(String iddonhang) {
+        AppMoMoLib.getInstance().setAction(AppMoMoLib.ACTION.PAYMENT);
+        AppMoMoLib.getInstance().setActionType(AppMoMoLib.ACTION_TYPE.GET_TOKEN);
+//        if (edAmount.getText().toString() != null && edAmount.getText().toString().trim().length() != 0)
+//            amount = edAmount.getText().toString().trim();
+
+        Map<String, Object> eventValue = new HashMap<>();
+        //client Required
+        eventValue.put("merchantname", merchantName); //Tên đối tác. được đăng ký tại https://business.momo.vn. VD: Google, Apple, Tiki , CGV Cinemas
+        eventValue.put("merchantcode", merchantCode); //Mã đối tác, được cung cấp bởi MoMo tại https://business.momo.vn
+        eventValue.put("amount", amount); //Kiểu integer
+        eventValue.put("orderId", iddonhang); //uniqueue id cho BillId, giá trị duy nhất cho mỗi BILL
+        eventValue.put("orderLabel", iddonhang); //gán nhãn
+
+        //client Optional - bill info
+        eventValue.put("merchantnamelabel", "Dịch vụ");//gán nhãn
+        eventValue.put("fee", "0"); //Kiểu integer
+        eventValue.put("description", description); //mô tả đơn hàng - short description
+
+        //client extra data
+        eventValue.put("requestId",  merchantCode+"merchant_billId_"+System.currentTimeMillis());
+        eventValue.put("partnerCode", merchantCode);
+        //Example extra data
+        JSONObject objExtraData = new JSONObject();
+        try {
+            objExtraData.put("site_code", "008");
+            objExtraData.put("site_name", "CGV Cresent Mall");
+            objExtraData.put("screen_code", 0);
+            objExtraData.put("screen_name", "Special");
+            objExtraData.put("movie_name", "Kẻ Trộm Mặt Trăng 3");
+            objExtraData.put("movie_format", "2D");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        eventValue.put("extraData", objExtraData.toString());
+
+        eventValue.put("extra", "");
+        AppMoMoLib.getInstance().requestMoMoCallBack(this, eventValue);
+
+
+    }
+    //Get token callback from MoMo app an submit to server side
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == AppMoMoLib.getInstance().REQUEST_CODE_MOMO && resultCode == -1) {
+            if(data != null) {
+                if(data.getIntExtra("status", -1) == 0) {
+                    //TOKEN IS AVAILABLE
+//                    tvMessage.setText("message: " + "Get token " + data.getStringExtra("message"));
+                    Toast.makeText(getApplicationContext(), data.getStringExtra("message"), Toast.LENGTH_SHORT).show();
+                    String token = data.getStringExtra("data"); //Token response
+                    String phoneNumber = data.getStringExtra("phonenumber");
+                    String env = data.getStringExtra("env");
+                    if(env == null){
+                        env = "app";
+                    }
+
+                    if(token != null && !token.equals("")) {
+                        // TODO: send phoneNumber & token to your server side to process payment with MoMo server
+                        // IF Momo topup success, continue to process your order
+                    } else {
+                        Toast.makeText(getApplicationContext(), "thatbai", Toast.LENGTH_SHORT).show();
+                    }
+                } else if(data.getIntExtra("status", -1) == 1) {
+                    //TOKEN FAIL
+                    String message = data.getStringExtra("message") != null?data.getStringExtra("message"):"Thất bại";
+                    Toast.makeText(getApplicationContext(), "thatbai", Toast.LENGTH_SHORT).show();
+
+                } else if(data.getIntExtra("status", -1) == 2) {
+                    //TOKEN FAIL
+                    Toast.makeText(getApplicationContext(), "thatbai", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //TOKEN FAIL
+                    Toast.makeText(getApplicationContext(), "thatbai", Toast.LENGTH_SHORT).show();
+
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "thatbai", Toast.LENGTH_SHORT).show();
+
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "thatbai", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
